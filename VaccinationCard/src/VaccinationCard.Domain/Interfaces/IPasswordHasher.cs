99eef99b78c81374c9b0528aae7fec1f1eb231b4
Henricky Monteiro <henrickyl1@gmail.com>
2026-06-24 23:16:49 -0300

    feature: add Bcrypt and JWT

---
+++ VaccinationCard/src/VaccinationCard.Domain/Interfaces/IPasswordHasher.cs
@@ -0,0 +1,7 @@
+﻿namespace VaccinationCard.Domain.Interfaces;
+
+public interface IPasswordHasher
+{
+    string Hash(string password);
+    bool Verify(string passwordHash, string password);
+}
---
+++ VaccinationCard/src/VaccinationCard.Infrastructure/Security/BCryptPasswordHasher.cs
@@ -0,0 +1,12 @@
+﻿using VaccinationCard.Domain.Interfaces;
+
+namespace VaccinationCard.Infrastructure.Security;
+
+public class BCryptPasswordHasher: IPasswordHasher
+{
+    public string Hash(string password)
+           => BCrypt.Net.BCrypt.HashPassword(password, workFactor: 12);
+
+    public bool Verify(string hash, string password)
+        => BCrypt.Net.BCrypt.Verify(password, hash);
+}
