1a41b6e4aef5650f763648dfd152e3f031c3839f
Henricky Monteiro <henrickyl1@gmail.com>
2026-06-25 03:44:04 -0300

    feature: add VaccineRepository

---
+++ VaccinationCard/src/VaccinationCard.Domain/Repositories/IVaccineRepository.cs
@@ -0,0 +1,14 @@
+﻿using VaccinationCard.Domain.Entities;
+
+namespace VaccinationCard.Domain.Repositories;
+
+public interface IVaccineRepository
+{
+    Task<Vaccine?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<IEnumerable<Vaccine>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<bool> ExistsByNameAsync(string name, CancellationToken cancellationToken = default);
+    Task<bool> ExistsByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    void Add(Vaccine vaccine);
+    void Update(Vaccine vaccine);
+    void Delete(Vaccine vaccine);
+}
