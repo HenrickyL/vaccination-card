428ab80f2c880f9a573c9f7e9b62836cc6df0289
Henricky Monteiro <henrickyl1@gmail.com>
2026-06-25 17:39:53 -0300

    feature: ListAllUsers

---
+++ VaccinationCard/src/VaccinationCard.Application/UseCases/Users/ListAllUsersResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace VaccinationCard.Application.UseCases.Users;
+
+public record ListAllUsersResponse
+{
+    public Guid UserId { get; init; }
+    public string Email { get; init; } = string.Empty;
+    public string Role { get; init; } = string.Empty;
+    public bool IsActive { get; init; }
+    public DateTime CreatedAt { get; init; }
+    public DateTime? UpdatedAt { get; init; }
+
+    public PatientInfoDto? Patient { get; init; }
+}
+
+public record PatientInfoDto
+{
+    public Guid Id { get; init; }
+    public string FullName { get; init; } = string.Empty;
+    public string IdentificationNumber { get; init; } = string.Empty;
+}
