61ebc6121eba734178a86bdd28dec61ad9a7c4b4
Henricky Monteiro <henrickyl1@gmail.com>
2026-06-25 00:31:42 -0300

    feature: add jwt i apio

---
+++ VaccinationCard/src/VaccinationCard.Api/Extensions/JwtExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace VaccinationCard.Api.Extensions;
+
+public static class JwtExtensions
+{
+    public static IServiceCollection AddJwtAuthentication(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+
+        var secret = configuration["Jwt:Secret"] ??
+                 Environment.GetEnvironmentVariable("JWT_SECRET") ??
+                 "SuperSecretKey1234567890!@#$";
+
+        var key = Encoding.ASCII.GetBytes(secret);
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
+        services.AddAuthorization();
+
+        return services;
+    }
+
+    public static IApplicationBuilder UseJwtAuthentication(
+        this IApplicationBuilder app)
+    {
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        return app;
+    }
+}
